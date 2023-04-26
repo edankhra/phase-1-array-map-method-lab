6825c9731b6ddc4f9210fbcb75669a495fae9aa0
Ekta <edankhra@gmail.com>
2023-04-25 22:08:24 -0400

    completed

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+
+
+
+
+  function titleCased(){
+    const capTutorials = tutorials.map(tutorial => {
+      const words = tutorial.split(" ").map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }).join(" ");
+      console.log(words);
+      return words; 
+    })
+    return capTutorials;
+  }
